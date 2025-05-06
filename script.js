fba75bc27f17a8883cba6ec66c2ea7b0de4f67c4
kaykenzo <kwilliamsalston2024@afscholars.org>
2025-05-06 10:00:30 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
+
+// 1. Use a for loop to display each fruit to the console.
+for(let i = 0;i<fruits.lenght;i++){
+  console.log(fruits[i])
+}
+
+
+let horrorMovies = [
+    "The Exorcist",
+    "Halloween",
+    "A Nightmare on Elm Street",
+    "The Shining",
+    "The Texas Chain Saw Massacre",
+    "Night of the Living Dead"
+  ];
+// 2. Use a for loop to display the first three items in the horrorMovies array to the console..
+for (let i = 0; i < 3; i++) {
+    console.log(horrorMovies[i]);
+}
+
+
+
+// 3. Can you create a loop that only does the last three in the array? Try it out.
+for (let i = horrorMovies.length - 3; i < horrorMovies.length; i++) {
+    console.log(horrorMovies[i]);
+}
